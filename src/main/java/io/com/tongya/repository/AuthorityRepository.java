440cbc6ebba942d764ef6e10fb2676c1e15b8c7d
Bhupendra Patel <bhupendrapatel1992@gmail.com>
2022-12-11 14:13:33 +0530

    Initail commit.

---
+++ src/main/java/io/com/tongya/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package io.com.tongya.repository;
+
+import io.com.tongya.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
